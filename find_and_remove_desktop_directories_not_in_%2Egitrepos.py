83816441c8843627fb9421f0efa306ae5c13cc7e
Daniel P Pflager <daniel.pflager@gmail.com>
2026-07-02 16:26:10 -0700

    progress

--- find_and_remove_desktop_directories_not_in_%2Egitrepos.py
+++ find_and_remove_desktop_directories_not_in_%2Egitrepos.py
@@ -20,16 +20,15 @@ gitrepos_dirs = []
 with open('../.gitrepos', 'r') as f:
     for line in f:
         line = line.rstrip('\n')
-        gitrepos_dirs.append(line.split()[0].replace("/", ""))
-
-print(gitrepos_dirs)
+        gitrepos_dirs.append(line[0])
 
 p = Path("../")
-dirs = [str(d).replace("../", "") for d in p.iterdir() if d.is_dir()]
-
-print(f"dirs == {dirs}")
+dirs = [d for d in p.iterdir() if d.is_dir()]
 
 for dir in dirs:
-    # print(f"dir == {dir}")
+    print(f"dir == {dir}")
     if dir not in gitrepos_dirs:
-        print(dir)
+        if confirm(f"Do you really want to delete directory {dir} recursively?"):
+            print("Deleting...")
+            shutil.rmtree(dir, ignore_errors=True)
+            print(f"Deleted {dir}")
